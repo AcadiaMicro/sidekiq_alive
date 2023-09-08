b29436d8008518f0b9207b749d5f8b6d4b82379c
Joshua McGee <jmcgee@ampion.net>
2023-09-08 08:09:10 -0400

    BAP-1823 Adding timeout code

--- lib/sidekiq_alive/worker.rb
+++ lib/sidekiq_alive/worker.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
+require "timeout"
+
 module SidekiqAlive
   class Worker
     include Sidekiq::Worker
     sidekiq_options retry: true
 
+    TIMEOUT = 30
+
     # Passing the hostname argument it's only for debugging enqueued jobs
     def perform(_hostname = SidekiqAlive.hostname)
-      # Checks if custom liveness probe passes should fail or return false
-      unless config.custom_liveness_probe.call
-        Rails.logger.error("SidekiqAlive: failed liveness probe, returning")
-        return
-      end
+      Timmeout.timeout(TIMEOUT) do
+        # Checks if custom liveness probe passes should fail or return false
+        unless config.custom_liveness_probe.call
+          Rails.logger.error("SidekiqAlive: failed liveness probe, returning")
+          return
+        end
 
-      # Writes the liveness in Redis
-      write_living_probe
-      # schedules next living probe
-      self.class.perform_in(config.time_to_live / 2, current_hostname)
+        # Writes the liveness in Redis
+        write_living_probe
+        # schedules next living probe
+        self.class.perform_in(config.time_to_live / 2, current_hostname)
+      end
     rescue => e
       Rails.logger.error("SidekiqAlive: exception raised: #{e}")
       raise e
